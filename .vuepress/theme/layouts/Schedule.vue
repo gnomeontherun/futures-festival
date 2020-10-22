547f9823fb99df5c86574ccee9c5e7c804a49321
Jeremy Wilken <gnomeontherun@users.noreply.github.com>
2020-10-22 22:39:13 +0000

    add detail to schedule
    Signed-off-by: GitHub <noreply@github.com>

--- .vuepress/theme/layouts/Schedule.vue
+++ .vuepress/theme/layouts/Schedule.vue
@@ -38,7 +38,7 @@
 
         <table>
           <tr>
-            <th>Session</th>
+            <th style="max-width: 40%">Session</th>
             <th>Speakers</th>
             <th>Type</th>
             <th style="min-width: 120px">Start Time</th>
@@ -47,7 +47,7 @@
         <template v-for="session in roomOne">
           <tr>
             <td v-if="session.path"><router-link :to="session.path">{{session.title}}</router-link></td>
-            <td v-else>{{session.title}}</td>
+            <td v-else>{{session.title}}<template v-if="session.frontmatter.summary"><p><i>{{session.frontmatter.summary}}</i></p></template></td>
             <td><template v-for="(speaker, index) in session.frontmatter.speakers">{{speaker.speaker}}<template v-if="session.frontmatter.speakers.length > index + 1">,<br /></template></template></td>
             <td>{{session.frontmatter.type}}</td>
             <td>{{session.frontmatter.date | localTime}}<br /><small>({{session.frontmatter.date | time}} EST)</small></td>
@@ -60,7 +60,7 @@
 
         <table>
           <tr>
-            <th>Session</th>
+            <th style="max-width: 40%">Session</th>
             <th>Speakers</th>
             <th>Type</th>
             <th style="min-width: 120px">Start Time</th>
@@ -69,7 +69,7 @@
         <template v-for="session in roomTwo">
           <tr>
             <td v-if="session.path"><router-link :to="session.path">{{session.title}}</router-link></td>
-            <td v-else>{{session.title}}</td>
+            <td v-else>{{session.title}}<template v-if="session.frontmatter.summary"><p><i>{{session.frontmatter.summary}}</i></p></template></td>
             <td><template v-for="(speaker, index) in session.frontmatter.speakers">{{speaker.speaker}}<template v-if="session.frontmatter.speakers.length > index + 1">,<br /></template></template></td>
             <td>{{session.frontmatter.type}}</td>
             <td>{{session.frontmatter.date | localTime}}<br /><small>({{session.frontmatter.date | time}} EST)</small></td>
@@ -97,22 +97,22 @@ import { resolveSidebarItems } from '../util'
 const sessionsRoomOne = [
   { title: 'Introduction', frontmatter: { date: '2020-10-24T07:00:00-04:00', type: 'Plenary', speakers: [{ speaker: 'Prateeksha Singh'}, { speaker: 'Graciela Guadarrama'}] }},
   { title: 'Breathing & Meditation', frontmatter: { date: '2020-10-24T09:15:00-04:00', type: 'Break', speakers: [{ speaker: 'Krittika Sharma'}] }},
-  { title: 'Reflection: Obituary to Self', frontmatter: { date: '2020-10-24T11:00:00-04:00', type: 'Reflection', speakers: [{ speaker: 'Krittika Sharma'}] }},
+  { title: 'Reflection: Obituary to Self', frontmatter: { summary: ' In this guided reflection session, Krittika Sharma will take you through an inward reflection on how -  Trying times stretch and test the human spirit. Trying times also bring us gifts. The gift of the pause. The gift to go inward. The gift to distinguish what is truly important. The gift to create meaning. In times like these, we must remember that we are not alone in this human experience. This gift is an invitation to explore our beautifully designed mortal spirit and unearth meaning.', date: '2020-10-24T11:00:00-04:00', type: 'Reflection', speakers: [{ speaker: 'Krittika Sharma'}] }},
   //{ title: 'Break', frontmatter: { date: '2020-10-24T11:45:00-04:00', type: 'Break', speakers: [{ speaker: ''}] }},
-  { title: 'Queer resilience admist uncertainty', frontmatter: { date: '2020-10-24T13:30:00-04:00', type: 'Conversation', speakers: [{ speaker: 'Lydia Timlin-Broussard'}], facilitators: 'Dave Roselle' }},
+  { title: 'Queer resilience admist uncertainty', frontmatter: { summary: 'Lydia will have an open conversation on queer resilience and what it means in a future with increasing uncertainty.', date: '2020-10-24T13:30:00-04:00', type: 'Conversation', speakers: [{ speaker: 'Lydia Timlin-Broussard'}], facilitators: 'Dave Roselle' }},
   { title: 'Breathing & Meditation', frontmatter:{ date: '2020-10-24T16:00:00-04:00', type: 'Break', speakers: [{ speaker: 'Amy Yockus Hartman'}] }},
   { title: 'Back to Back Listening', frontmatter: { date: '2020-10-24T18:15:00-04:00', type: 'Reflection', speakers: [{ speaker: 'Zainab Kakal'}] }},
   { title: 'Closing', frontmatter: { date: '2020-10-24T18:30:00-04:00', type: 'Plenary', speakers: [{ speaker: 'Amy Hosotsuji'}] }},
 ];
 
 const sessionsRoomTwo = [
-  { title: 'Introduction', frontmatter: { date: '2020-10-24T07:00:00-04:00', type: 'Plenary', speakers: [{ speaker: 'Prateeksha Singh'}, { speaker: 'Graciela Guadarrama'}] }},
+  { title: 'Introduction', frontmatter: { summary: 'Use Room 1 live stream', date: '2020-10-24T07:00:00-04:00', type: 'Plenary', speakers: [{ speaker: 'Prateeksha Singh'}, { speaker: 'Graciela Guadarrama'}] }},
   { title: 'Energizer', frontmatter: { date: '2020-10-24T09:00:00-04:00', type: 'Break', speakers: [{ speaker: ''}] }},
   { title: 'Reflection: Obituary to Self', frontmatter: { date: '2020-10-24T11:00:00-04:00', type: 'Reflection', speakers: [{ speaker: 'Krittika Sharma'}] }},
-  { title: 'Mental Health', frontmatter: { date: '2020-10-24T14:00:00-04:00', type: 'Conversation', speakers: [{ speaker: 'Sheila Mutaramuka'}] }},
+  { title: 'How to keep your mental health in check', frontmatter: { summary: ' In this conversation session, Sheila Mutaramuka, Creative Director of Maqoba, will talk with about: A practical 5 minute activity of how to calm yourself down in stressful situations at work and in daily life. Open Dialogue on why mental health is more important now than ever. Tips and tricks to help maintain your mental health', date: '2020-10-24T14:00:00-04:00', type: 'Conversation', speakers: [{ speaker: 'Sheila Mutaramuka'}] }},
   { title: 'Energizer', frontmatter:{ date: '2020-10-24T16:15:00-04:00', type: 'Break', speakers: [{ speaker: 'Amy Yockus Hartman'}] }},
-  { title: 'Breathing & Meditation', frontmatter: { date: '2020-10-24T18:15:00-04:00', type: 'Reflection', speakers: [{ speaker: 'Zainab Kakal'}] }},
-  { title: 'Closing', frontmatter: { date: '2020-10-24T18:30:00-04:00', type: 'Plenary', speakers: [{ speaker: 'Amy Hosotsuji'}] }},
+  { title: 'Breathing & Meditation', frontmatter: { summary: 'Use Room 1 live stream', date: '2020-10-24T18:15:00-04:00', type: 'Reflection', speakers: [{ speaker: 'Zainab Kakal'}] }},
+  { title: 'Closing', frontmatter: { summary: 'Use Room 1 live stream', date: '2020-10-24T18:30:00-04:00', type: 'Plenary', speakers: [{ speaker: 'Amy Hosotsuji'}] }},
 ];
 
 export default {
